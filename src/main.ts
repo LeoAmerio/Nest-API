0e994f6d55751ea18eddfa3179def1424415a21a
Leonardo Amerio <leoamerio14@gmail.com>
2024-03-08 13:42:25 -0300

    Add Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     // transform: true
   }));
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API Store')
+    .setDescription('API Store using NestJS and Swagger')
+    .setVersion('1.0')
+    .addTag('store')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3002);
 }
 bootstrap();
